8ba9b2558bcffbf45e67361539e8f89b8de3f5b7
farhan <116582187+frhanjav@users.noreply.github.com>
2023-07-01 23:15:27 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,16 @@
+// WAP to print your name
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Farhan Javed\n");
+    
+    int i, j =0;
+    int c, d =2;
+    int e =3;
+    int f,g;
+    printf("%d %d %d %d %d %d %d " ,i, j,c,d,e,f,g);
+
+    return 0;
+}
